ce66609529bd9a25a68acfdffcf7144a7cc6dc4c
IgorViniciosAV <159195982+IgorViniciosAV@users.noreply.github.com>
2024-05-28 10:05:25 -0300

    Atualizar o script.js
    ajustar tempo foco

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const audioPlay = new Audio('./sons/play.wav');
 const audioPausa = new Audio('./sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
-let tempoDecorridoEmSegundos = 15000
+let tempoDecorridoEmSegundos = 1500
 let intervaloId = null
 
 musica.loop = true
@@ -38,7 +38,7 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 15
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
